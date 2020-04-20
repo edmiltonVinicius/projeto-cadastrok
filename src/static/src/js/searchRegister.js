8fbbf94f9928444cf87421160d597cfa094a8e0c
Edmilton Vinicius <edmilton.vinicius2@gmail.com>
2020-04-20 01:00:33 -0300

    tela de loading pronta

--- src/static/src/js/searchRegister.js
+++ src/static/src/js/searchRegister.js
@@ -1,14 +1,30 @@
 const myLink = document.getElementById('btnRegister-js')
-
 function stopPadrao(event) {
     event.preventDefault()
 } 
 
+const divLogin = document.querySelector('.cont')
+const divLoad = document.querySelector('.loading')
+
+function showLoading(){
+    divLogin.style.display='none'
+    divLoad.style.display='block'
+}
+
+function showAnswer(res){
+    divLoad.style.display='none'
+    divLogin.innerHTML=res.data
+    divLogin.style.display='block'
+} 
+
 function funcRegister() {
-    axios.get('/register')
-    .then((res) => {
-        document.querySelector('.cont').innerHTML=res.data
-    })
+    showLoading()
+    setInterval(() => {
+        axios.get('/register')
+            .then((res) => {
+                showAnswer(res)
+            })
+    }, 2000);
 }
 
 myLink.addEventListener('click', funcRegister)
