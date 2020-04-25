cf10e1fa84d124adcf0c6e5105d53cce2ecc2a12
Edmilton Vinicius <edmilton.vinicius2@gmail.com>
2020-04-25 19:29:26 -0300

    cadastrado de usuarios funcionando e devolvendo um alert com as informações

--- src/routes/router-register.js
+++ src/routes/router-register.js
@@ -6,16 +6,18 @@ const User = require('../database/user-schema')
 router.post('/', (req, res) => {
     const { userName, userPassword, userEmail} = req.body
 
-    User.findOne({userName}, (err, arq) => {
-        if(err) res.json({message: 'Error occurred, try again'})
-        if(arq) res.json({message: 'User already Registered'})
+    User.findOne({email: userEmail}, (err, arq) => {
+        if(err) return res.json({message: 'Error occurred, try again'})
+
+        if(arq) return res.json({message: 'Email already Registered'})
+         
         const newUser = new User({
             name: userName,
             password: userPassword,
             email: userEmail
         })
         newUser.save(err => {
-            if(err) res.json({message: 'Erro on Register'})
+            if(err) return res.json({message: 'Erro on Register'})
             res.json({message: 'Register Successfully'})
         })
     })
