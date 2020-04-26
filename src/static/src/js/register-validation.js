fd42cdcd7ac186dee4532142c42de099728ff2a5
Edmilton Vinicius <edmilton.vinicius2@gmail.com>
2020-04-25 23:25:56 -0300

    validacao das entradas funcionando

--- src/static/src/js/register-validation.js
+++ src/static/src/js/register-validation.js
@@ -1,20 +1,21 @@
 function validationInput(id, q) {
     const input = document.getElementById(id).value
     const camp = document.getElementById(id)
-
-    if (input.length > Number(q)) {
+    
+    if (input.length >= Number(q)) {
         camp.style.borderColor = "green"
         camp.style.background = "url('../static/build/img/certo.png') 95% 50% no-repeat"
-
+        
     } else if(input.length == ''){
         camp.classList.add('border-light')
-     
+        
     } else {
         camp.classList.remove('border-light')
         camp.style.background = "none"
-        camp.style.borderColor = "red"
+        camp.style.borderColor = "red"  
     }
 }
+
 
 function validationEmail(){
     const input = document.getElementById('entEmail').value
@@ -30,13 +31,42 @@ function validationEmail(){
     ){
         camp.style.borderColor="green"
         camp.style.background="url('../static/build/img/certo.png') 95% 50% no-repeat"
+        verificationButtonCreate()
 
     } else if(input.length == 0) {
         camp.classList.add('border-light')
+        disableButtonCreate()
+        
     } else {
         camp.classList.remove('border-light')
         camp.style.borderColor="red"
-        camp.style.background="none"
+        camp.style.background="none" 
+        disableButtonCreate()
     }
+}
 
+
+const buttonCreate = document.querySelector('.btnCreate-js')
+
+function disableButtonCreate(){
+    buttonCreate.setAttribute('disabled', 'disabled')
+    buttonCreate.classList.remove('btn-outline-light')
+    buttonCreate.classList.add('btn-outline-secondary')
+}
+
+function enableButtonCreate(){
+    buttonCreate.removeAttribute('disabled')
+    buttonCreate.classList.add('btn-outline-light')
+}
+
+function verificationButtonCreate(){
+    setInterval(() => {
+        if( (document.getElementById('entName').value.length >= 3) &&
+            (document.getElementById('entPass').value.length >= 6) &&
+            (document.getElementById('entEmail').style.borderColor == 'green') ){
+            enableButtonCreate()
+        } else {
+            disableButtonCreate()
+        }
+    }, 100);
 }
