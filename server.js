b671dd82a5952d60534c20e57d3aa87be33d4411
Edmilton Vinicius <edmilton.vinicius2@gmail.com>
2020-04-27 20:15:06 -0300

    retirado body-parser incluido express.json

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-/*require('dotenv').config()
-global.banco = require('./src/database/connection')*/
+require('dotenv').config()
+global.banco = require('./src/database/connection')
 
 
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
 
 const routerLogin = require('./src/routes/router-login')
@@ -13,8 +12,9 @@ const routerRegister = require('./src/routes/router-register')
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/src/views')
 app.use('/static', express.static(__dirname + '/src/static/'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 
 app.use('/', routerLogin)
