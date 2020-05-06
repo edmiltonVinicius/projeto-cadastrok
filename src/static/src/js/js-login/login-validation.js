5a8902418ce9510fe6ec46b5c7fc5059202e7bcd
Edmilton Vinicius <edmilton.vinicius2@gmail.com>
2020-05-05 23:39:13 -0300

    gif loading funcionando

--- src/static/src/js/js-login/login-validation.js
+++ src/static/src/js/js-login/login-validation.js
@@ -10,26 +10,33 @@ const LoginUser = () => {
     const passUser = document.getElementById('pass').value
 
     const divErro = document.querySelector('.errorLogin')
-    divErro.classList.add('d-none')
+    const textErro = document.querySelector('.textErro')
+    const imgLoading = document.getElementById('imgLoading')
+
+    textErro.classList.add('d-none')
+    divErro.classList.remove('d-none')
+    imgLoading.classList.remove('d-none')
 
     axios.post('/', { loginUser, passUser })
         .then(res => {
             setTimeout(() => {
                 if(res.data.message === 'valid data' && res.data.code === 200){
                     sessionStorage.setItem('token', res.data.token)
+                    imgLoading.classList.add('d-none')
                     window.location.replace('/dashboard')
     
                 } 
                 if(res.data.code === 401){
-                    divErro.innerHTML=res.data.message
-                    divErro.classList.remove('d-none')
+                    imgLoading.classList.add('d-none')
+                    textErro.innerHTML=res.data.message
+                    textErro.classList.remove('d-none')
                     
                 }
             }, 1000);
         })
         .catch(err => {
-            divErro.innerHTML=res.data.message
-            divErro.classList.remove('d-none')
+            textErro.innerHTML=res.data.message
+            textErro.classList.remove('d-none')
         })
 
 }
