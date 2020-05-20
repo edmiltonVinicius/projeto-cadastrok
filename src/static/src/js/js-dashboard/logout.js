b8a7756b2ddc45a8383f44b1f34a2b99e86b5452
Edmilton Vinicius <edmilton.vinicius2@gmail.com>
2020-05-20 00:05:47 -0300

    cancel funcionando porem com bug

--- src/static/src/js/js-dashboard/logout.js
+++ src/static/src/js/js-dashboard/logout.js
@@ -2,3 +2,14 @@ const logout = () => {
     sessionStorage.removeItem('token')
     window.location.replace('/')
 }
+
+const modal = document.getElementById('modalDeLogout')
+
+const cancel = () => {
+    modal.style.display='none'
+    document.querySelector('.modal-backdrop').classList.add('d-none')
+}
+
+const reappear = () => {
+    modal.style.display='block'
+}
