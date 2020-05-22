f98b8ae7af5692b5739125fabd785f2c631c7369
Edmilton Vinicius <edmilton.vinicius2@gmail.com>
2020-05-21 23:36:23 -0300

    mandando os dados do user ao logar

--- src/routers/router-dashboard.js
+++ src/routers/router-dashboard.js
@@ -2,9 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 const middlleware = require('./middleware-jwt')
+const User = require('../database/user-schema')
 
 router.get('/', middlleware, (req, res) => {
-    res.status(200).render('layouts/dashboard')
+    const idUser = req.idUser
+    User.find({_id:idUser}, {_id: 0, password: 0}, (err, result) => {
+        if(err) console.log(err)
+        res.status(200).render('layouts/dashboard', {user: result[0]})
+    })
 })
 
 
