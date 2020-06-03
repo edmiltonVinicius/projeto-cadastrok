1116b05e10f261fc7eb12c667a5f1dca4d1288cb
Edmilton Vinicius <edmilton.vinicius2@gmail.com>
2020-06-03 01:52:48 -0300

    voltei a config do multer para a rota e voltei a exclusao fs

--- src/routers/router-upload-img.js
+++ src/routers/router-upload-img.js
@@ -1,16 +1,42 @@
 const express = require('express')
 const router = express.Router()
-//const fs = require('fs')
+const fs = require('fs')
+const multer = require('multer')
 const cloudinary = require('cloudinary').v2
 const middleware = require('./middleware-jwt')
 const User = require('./../database/user-schema')
 
 const cloudinaryConfig = require('../config/config-cloudinary')
-const multerConfig = require('../config/config-multer')
+//const multerConfig = require('../config/config-multer')
 
 cloudinary.config(cloudinaryConfig) 
 
-router.post('/', middleware, multerConfig.single('file'), (req, res) => {
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'src/uploads-img-users/')
+    },
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+const fileFitler = (req, file, cb) => {
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png'){
+        cb(null, true)
+    }
+    cb(null, false)
+}
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5,
+        files: 1
+    },
+    fileFilter: fileFitler
+})
+
+router.post('/', middleware, upload.single('file'), (req, res) => {
     const file = req.file.path
     const idUser = req.idUser
     if(file){
@@ -22,11 +48,11 @@ router.post('/', middleware, multerConfig.single('file'), (req, res) => {
                 {image: { publicId : result.public_id, secureUrl : result.secure_url}},
                 { new: true }, (err, arq) => {
                     if(err) return res.status(500).send('Error on Mongoose.')
-                    /*fs.unlink(file, (err) => {
+                    fs.unlink(file, (err) => {
                         if(err) {
                             return res.status(500).send('Sorry, an error has occurred.')
                         }
-                    })*/
+                    })
                     return res.status(201).send(arq.image.secureUrl)
             })
         })
