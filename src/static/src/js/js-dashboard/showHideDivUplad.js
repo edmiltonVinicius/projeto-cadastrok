a3bc3d554536b7b9f06b04fafa5cf57840c16894
Edmilton Vinicius <edmilton.vinicius2@gmail.com>
2020-06-25 03:51:22 -0300

    varaias melhorias

--- src/static/src/js/js-dashboard/showHideDivUplad.js
+++ src/static/src/js/js-dashboard/showHideDivUplad.js
@@ -1,63 +1,40 @@
-const showHideDivUplad = (hide, show, efect) => {
-    const divHide = document.querySelector(hide)
-    const divShow = document.querySelector(show)
-    const divEfect = document.querySelector(efect)
+const showHideDivUplad = (efect, show, hide) => {
+    // Array de elementos para esconder
+    const divsHide = hide
+    //Array de elementos para mostrar
+    const divsShow = show
     
     const inputFile = document.getElementById('upladoImage')
-    const divLoadingUpload = document.querySelector('.loadingUpload')
-    const divPreviewImg = document.querySelector('.divPreviewImg')
     const previewImg = document.getElementById('previewImg')
     
-    const iconRight = document.getElementById('iconRight')
-    const gifUpload = document.getElementById('gifUpload')
-
-    gifUpload.classList.remove('d-inline')
-    gifUpload.classList.add('d-none')
-
-    iconRight.classList.remove('d-inline')
-    iconRight.classList.add('d-none')
-    
+    hideElement('gifUpload')
+    hideElement('iconRightUpload')
 
     if(efect){
-        divShow.classList.remove('d-block')
-        divShow.classList.add('d-none')
+        divsHide.forEach(e => hideElement(e))
         
-        divHide.classList.remove('d-block')
-        divHide.classList.add('d-none')
-
         inputFile.value=''
-        divPreviewImg.classList.remove('d-block')
-        divPreviewImg.classList.add('d-none')
+        hideElement('.divPreviewImg')
         previewImg.classList.add('d-none')
         clearInputAndDiv.classList.add('d-none')
         
-        divEfect.classList.remove('d-none')
-        divEfect.classList.add('d-block')
-
+        showElement(efect)
+        
         setTimeout(() => {
-            divEfect.classList.remove('d-block')
-            divEfect.classList.add('d-none')
-            divShow.classList.remove('d-none')
-            divShow.classList.add('d-block')
+            hideElement(efect)
+            divsShow.forEach(e => showElement(e))
         }, 1500);
          
     } else {
-        divLoadingUpload.classList.remove('d-block')
-        divLoadingUpload.classList.add('d-none')
-
-        divPreviewImg.classList.remove('d-block')
-        divPreviewImg.classList.add('d-none')
+        hideElement('.loadingUpload')
+        hideElement('.divPreviewImg')
 
         inputFile.value=''
         previewImg.classList.add('d-none')
         clearInputAndDiv.classList.add('d-none')
 
-        if(divHide || divShow){
-            divHide.classList.remove('d-block')
-            divHide.classList.add('d-none')
-    
-            divShow.classList.remove('d-none')
-            divShow.classList.add('d-block')
-        }
+        divsHide.forEach(e => hideElement(e))
+        divsShow.forEach(e => showElement(e))
+        
     }
 }
