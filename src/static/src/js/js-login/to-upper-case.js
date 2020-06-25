efad76d6bba6ae9e16a9d6614834594e335c663e
Edmilton Vinicius <edmilton.vinicius2@gmail.com>
2020-06-25 01:02:22 -0300

    componentizado toda a dashboard

--- src/static/src/js/js-login/to-upper-case.js
+++ src/static/src/js/js-login/to-upper-case.js
@@ -2,7 +2,7 @@ const upperText = (name) => {
     const arrayName = name.split(' ')
     
     const firstLetter = arrayName.map((e) => e.substring(0, 1).toUpperCase())
-    const restName = arrayName.map((e) => e.substring(1, e.lenght))
+    const restName = arrayName.map((e) => e.substring(1, e.lenght).toLowerCase())
 
     const nameEdited = firstLetter.map((e, f) => `${e}${restName[f]}`)
     const readyName = nameEdited.reduce((nomeFinal, valor) => `${nomeFinal} ${valor}`)
