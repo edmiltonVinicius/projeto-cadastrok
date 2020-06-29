e60d7f196c8a7ca1ac4febcc511b2eeacde0ee95
Edmilton Vinicius <edmilton.vinicius2@gmail.com>
2020-06-29 02:36:28 -0300

    deletada a pasta crud-operations

---
+++ src/routers/crud-operations-contact.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const middlleware = require('./middleware-jwt')
+const User = require('../database/user-schema')
+const router = express.Router()
+
+router.patch('/create', middlleware, (req, res) => {
+    const idUser = req.idUser
+    const { nameContact, numberContact, imageContact, sexoContact, emailContact } = req.body
+    
+    const contact = { nameContact, numberContact, imageContact, sexoContact, emailContact }
+    
+    User.findByIdAndUpdate(idUser, {$push: {contacts: contact }}, {new: true}, (err, arq) => {
+        if(err) return res.status(500).send('Sorry, there was an error, please try again.')
+   
+        return res.status(200).json(arq)            
+    })
+})
+
+module.exports = router
